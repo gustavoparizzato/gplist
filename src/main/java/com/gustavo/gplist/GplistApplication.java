bcd7169ce87165e3c6037cd456e1185ef09d19f6
Gustavo Parizzato <gustavoventura246@gmail.com>
2023-05-09 20:33:45 -0300

    Project Created and Properties, Game, ORM

---
+++ src/main/java/com/gustavo/gplist/GplistApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.gplist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GplistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GplistApplication.class, args);
+	}
+
+}
