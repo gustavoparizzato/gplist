3616ea9fb601a949c93cf8d3511d9214c9735368
Gustavo Parizzato <gustavoventura246@gmail.com>
2023-05-10 19:56:52 -0300

    GET game by id

--- src/main/java/com/gustavo/gplist/services/GameService.java
+++ src/main/java/com/gustavo/gplist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gustavo.gplist.dto.GameDTO;
 import com.gustavo.gplist.dto.GameMinDTO;
 import com.gustavo.gplist.entities.Game;
 import com.gustavo.gplist.repositories.GameRepository;
@@ -15,6 +17,13 @@ public class GameService {
 	@Autowired
 	private GameRepository repository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = repository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = repository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
